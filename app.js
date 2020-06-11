b3a5fb44aa4b20aa6b3f052cdc844f21d9854e33
Devinml <devinlink8@gmail.com>
2020-06-11 10:22:41 -0600

    using md5

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 //jshint esversion:6
 require('dotenv').config()
+const md5 = require('md5');
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+
 
 const app = express();
 
-console.log(process.env.API_KEY);
+
 
 app.set('view engine', 'ejs');
 
@@ -25,7 +26,6 @@ const userSchema = new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt,{secret : process.env.SECRET, encryptedFields:['password'] });
 
 const User = new mongoose.model('User', userSchema);
 
@@ -45,7 +45,7 @@ app.get('/register',function(req,res){
 app.post('/register', function(req,res){
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
     newUser.save(function(err){
         if(err){
@@ -58,7 +58,7 @@ app.post('/register', function(req,res){
 
 app.post('/login', function(req,res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email: username}, function(err, foundUser){
         if(err){
