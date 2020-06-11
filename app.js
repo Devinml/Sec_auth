08149a041689572f474df02dc596d57c66b48b83
Devinml <devinlink8@gmail.com>
2020-06-11 16:25:52 -0600

    added facebook authentication

--- app.js
+++ app.js
@@ -9,8 +9,11 @@ const bcrypt = require('bcrypt');
 const saltRounds = 10;
 const session = require('express-session');
 const passport = require('passport');
-const passportLocalMongoose = require('passport-local-mongoose')
+const passportLocalMongoose = require('passport-local-mongoose');
 const app = express();
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
+const FacebookStrategy = require('passport-facebook').Strategy;
 
 
 app.use(express.static("public"));
@@ -34,40 +37,124 @@ mongoose.set('useCreateIndex', true);
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String,
+    facebookId: String,
+    secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 
 const User = new mongoose.model('User', userSchema);
 
 passport.use(User.createStrategy());
  
-// use static serialize and deserialize of model for passport session support
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+});;
+
+passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_APP_ID,
+    clientSecret: process.env.FACEBOOK_APP_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/secrets"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+      console.log(profile)
+    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo',
+  },
+  function(accessToken, refreshToken, profile, cb) {
+      
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get('/',function(req,res){
     res.render('home.ejs');
     
 });
 
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] })
+);
+
+app.get('/auth/google/secrets', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+  });
+
 app.get('/login',function(req,res){
     res.render('login.ejs');
 });
+
+app.get('/submit',function(req,res){
+    if(req.isAuthenticated){
+        res.render('submit');
+    }else{
+        res.redirect('/login');
+    }
+});
+
+app.post('/submit',function(req,res){
+    const submittedSecret = req.body.secret;
+
+    User.findById(req.user.id,function(err,foundUser){
+        if(err){
+            console.log(err);
+        }else{
+            if(foundUser){
+                foundUser.secret = submittedSecret;
+                foundUser.save(function(){
+                    res.redirect('/secrets')
+                });
+            };
+        };
+    });
+});
+
+app.get('/auth/facebook',
+  passport.authenticate('facebook',{ scope: ['user_friends', 'manage_pages'] }));
+
+app.get('/auth/facebook/secrets',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
 
 app.get('/register',function(req,res){
     res.render('register.ejs');
 });
 
 app.get('/secrets', function(req,res){
-    
-    if(req.isAuthenticated()){
-        res.render('secrets');
-    }else{
-        res.redirect('/login');
-    }
+    User.find({'secret':{$ne:null}},function(err,foundUsers){
+        if(err){
+            console.log(err)
+        }else{
+            res.render('secrets',{usersWithSecrets:foundUsers})
+        }
+    })
 })
 
 app.post('/register', function(req,res){
